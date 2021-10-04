50fe8dc64615be0dded11d3f44e2898a289de952
Danny Tan <31455779+dtan23@users.noreply.github.com>
2021-10-04 16:16:07 -0700

    Message (Added ownership disclaimer)

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,1 +1,3 @@
 # Main file for Tic Tac Toe game
+# Please note that this code is based off of Aaron Bernath's TicTacToeApp and I do not in any way claim ownership of said code.
+# This code was found here: https://www.youtube.com/watch?v=BHh654_7Cmw and https://replit.com/@aaron_bernath/PythonTicTacToeApp
